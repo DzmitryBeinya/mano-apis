6701408cd5851b069546940a459e23cf35e5f6ab
dzbr1016 <dzmitry.brouka@netcracker.com>
2018-09-17 16:41:36 +0300

    Added OpenStackAuth endpoinst and OpenStackImagesController

--- src/main/java/com/netcracker/auth/OpenStackAuthController.java
+++ src/main/java/com/netcracker/auth/OpenStackAuthController.java
@@ -1,6 +1,8 @@
 package com.netcracker.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +16,21 @@ import java.nio.file.Paths;
 @RequestMapping("/v3/auth/tokens")
 public class OpenStackAuthController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET,produces = "application/json")
     @ResponseBody
-    public String getTokens() throws IOException {
+    public ResponseEntity<String> getTokens() throws IOException {
 
-        return new String(Files.readAllBytes(Paths.get("docs/auth.json")));
+        return new ResponseEntity<>(new String(Files.readAllBytes(Paths.get("responses/openstack-auth-get.json"))), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<String> getToken() throws IOException {
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("x-subject-token","gAAAAABbmxI_h7tD-cITN6D8mBvRlsB7B3ttZEWEdogQDpATpbGKGdmTTbcbSW3wIQ1_Ndvhrwnvu6Cwyt-juY-HIkgNJMX37MwbI3pszCK1pQsdxLNxuQAiW3t-1LSAxOr6Y2YJ74weS9gGEemNf805ZM8VxTAfbbjX00yxujFSzIP94e8UsVM");
+        httpHeaders.set("x-openstack-request-id","req-fe7c3e1f-f750-4934-bf15-78fa6732522c");
+        httpHeaders.set("vary","X-Auth-Token");
+        return new ResponseEntity<>(new String(Files.readAllBytes(Paths.get("responses/openstack-auth-post.json"))), httpHeaders, HttpStatus.OK);
     }
 }
--- src/main/java/com/netcracker/images/OpenStackImagesController.java
+++ src/main/java/com/netcracker/images/OpenStackImagesController.java
@@ -3,11 +3,20 @@ package com.netcracker.images;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @RestController
 public class OpenStackImagesController {
 
     @GetMapping(value = "/v2/images", produces = "application/json")
-    public String getImages() {
-        return "222";
+    public String getImages() throws IOException {
+        return new String(Files.readAllBytes(Paths.get("responses/openstack-images-get.json")));
+    }
+
+    @GetMapping(value = "/v2/schemas/image", produces = "application/json")
+    public String getSchemas() throws IOException {
+        return new String(Files.readAllBytes(Paths.get("responses/openstack-schemas-images-get.json")));
     }
 }
